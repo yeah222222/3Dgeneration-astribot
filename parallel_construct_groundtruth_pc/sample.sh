9a904579091da7a2b96b3368ed10a4b49c7b8567
yeah <107670944+yeah222222@users.noreply.github.com>
2024-09-26 17:38:42 +0800

    Delete parallel_construct_groundtruth_pc directory

--- parallel_construct_groundtruth_pc/sample.sh
+++
@@ -1,4 +0,0 @@
-python /home/ec2-user/zjc_renderobjaverse/sample_pc/distributed_sample.py\
-	--num_gpus 8 \
-	--workers_per_gpu 2 \
-	--input_models_path /home/ec2-user/zjc_renderobjaverse/objaverse_render/objaverse_data_zjc/hf-objaverse-v1/object-paths.json
